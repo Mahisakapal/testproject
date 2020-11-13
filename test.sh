5db3d4296b11daa873f88c254b385ef097b43a01
Mahisakapal <mahisakapal@gmail.com>
2020-11-13 23:44:44 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,22 +1,17 @@
-sudo apt install nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx 
-
 sudo systemctl enable nginx
 
 #instation of PHP
-sudo apt install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-apt --disablerepo="*" --enablerepo="remi-safe" list php[7-9][0-9].x86_64
-sudo apt-config-manager --enable remi-php74
-sudo apt install php php-mysqlnd php-fpm
+sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum --disablerepo="*" --enablerepo="remi-safe" list php[7-9][0-9].x86_64
+sudo yum-config-manager --enable remi-php74
+sudo yum install php php-mysqlnd php-fpm
 php --version
 systemctl start php-fpm.service
 systemctl enable php-fpm.service
 
 #installtion of sql sever 
 
-sudo apt install mariadb-server mariadb
+sudo yum install mariadb-server mariadb
 sudo systemctl start mariadb
 sudo mysql_secure_installation
 sudo systemctl enable mariadb
-
