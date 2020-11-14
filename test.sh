039726840f9ca2ed8681d546c353fa7f9e8b0b06
Mahisakapal <mahisakapal@gmail.com>
2020-11-14 17:54:31 +0530

    update file

--- test.sh
+++ test.sh
@@ -1,17 +1,28 @@
+#instation of ngnix
+sudo yum install ngnix -y
+sudo systemctl start nginx 
 sudo systemctl enable nginx
 
 #instation of PHP
-sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
 yum --disablerepo="*" --enablerepo="remi-safe" list php[7-9][0-9].x86_64
 sudo yum-config-manager --enable remi-php74
-sudo yum install php php-mysqlnd php-fpm
+sudo yum install php php-mysqlnd php-fpm -y
 php --version
 systemctl start php-fpm.service
 systemctl enable php-fpm.service
 
 #installtion of sql sever 
 
-sudo yum install mariadb-server mariadb
-sudo systemctl start mariadb
-sudo mysql_secure_installation
-sudo systemctl enable mariadb
+yum -y install @mysql 
+systemctl start mysqld
+systemctl enable --now mysqld
+systemctl status mysqld
+
+# this is only if you want to do security task 
+# mysql_secure_installation
+
+mysql -u root -p
+create database mahi;
+GRANT ALL ON tecmint.* TO mahi@test IDENTIFIED BY 'test123';
+exit
