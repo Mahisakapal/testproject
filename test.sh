6f8f9c4f1ac2b22c9abf3492c51b0b69e2aed7ab
Mahisakapal <mahisakapal@gmail.com>
2020-11-14 22:29:56 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,21 @@
+########################################################################
+# this is the script for install PHP 7.4 with mysql or mariyaDB
+#
+# and we use ngnix as web server  
+#
+#Note	: Befor ruing script please check once and run with super user
+########################################################################
+
 #instation of ngnix
+
+echo "instaling ngnix"
 sudo yum install ngnix -y
 sudo systemctl start nginx 
 sudo systemctl enable nginx
 
 #instation of PHP
+
+echo "instaling PHP 7.4"
 sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
 yum --disablerepo="*" --enablerepo="remi-safe" list php[7-9][0-9].x86_64
 sudo yum-config-manager --enable remi-php74
@@ -12,8 +24,9 @@ php --version
 systemctl start php-fpm.service
 systemctl enable php-fpm.service
 
-#installtion of sql sever 
+#installtion of sql  
 
+echo "instaling mysql"
 sudo yum install mysql -y
 sudo systemctl start mysqld
 sudo systemctl status mysqld
@@ -26,3 +39,12 @@ mysql -u root -p
 create database mahi;
 GRANT ALL ON tecmint.* TO mahi@test IDENTIFIED BY 'test123';
 exit
+
+#this is bkp database mariyaDB
+#sudo dnf install mariadb-server -y
+#sudo systemctl start mariadb
+#sudo mysql_secure_installation
+#sudo mysql
+#mysql -u %usr% -p
+#SHOW DATABASES;
+
