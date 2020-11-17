3ff63735dfbb0591c65ec945e1158ccb5ce57143
Mahisakapal <mahisakapal@gmail.com>
2020-11-17 14:33:26 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -24,6 +24,18 @@ php --version
 systemctl start php-fpm.service
 systemctl enable php-fpm.service
 
+
+# updateing PHP 7.2 to PHP 7.4
+
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm
+dnf module list php
+dnf module reset php:remi-7.4
+dnf module enable php:remi-7.4
+dnf install php php-cli php-common
+php -v
+
+
 #installtion of sql  
 
 echo "instaling mysql-server"
