1435c17e43f7c36d25f26e188bcfe6d56573eca2
Mahisakapal <mahisakapal@gmail.com>
2020-11-19 12:56:13 +0530

    Update bkp.sh

--- bkp.sh
+++ bkp.sh
@@ -67,3 +67,9 @@ Phpmyadmin installation use the below command
 For allowing the access the phpmyadmin please follow the video to granted
 
 File location : /etc/httpd/conf.d/phpMyAdmin.conf
+
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf module list php
+ dnf module enable php:remi-7.4 -y 
+ dnf install php php-cli php-common -y 
